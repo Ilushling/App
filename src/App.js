b47d4fca4ef30dcd3543d948d5478fadd6f03b53
Ilushling <7870818+Ilushling@users.noreply.github.com>
2023-12-28 18:38:39 +0000

    feat: secrets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 /**
- * @typedef {{ setup?: (params?: { configs: unknown, resources: unknown }) => any }} Component
+ * @typedef {{ setup?: (params?: Partial<{ configs: unknown, resources: unknown, secrets: unknown }>) => any }} Component
  * @typedef {Record<string, Component>} Components
  * 
- * @typedef {{ run: (params?: { configs: unknown, resources: unknown }) => any }} AppInterface
+ * @typedef {{ run: (params?: Partial<{ configs: unknown, resources: unknown, secrets: unknown }>) => any }} AppInterface
  * @typedef {Record<string, AppInterface>} Interfaces
  * 
  * @typedef {object} Configs
@@ -19,6 +19,11 @@
  * @typedef {Record<string, unknown>} ComponentResources
  * @typedef {Record<string, unknown>} InterfaceResources
  * 
+ * @typedef {object} Secrets
+ * @property {ComponentSecrets=} components
+ * @property {InterfaceSecrets=} interfaces
+ * @typedef {Record<string, unknown>} ComponentSecrets
+ * @typedef {Record<string, unknown>} InterfaceSecrets
  */
 
 /**
@@ -28,6 +33,7 @@
  * 
  * @property {Configs=} configs
  * @property {Resources=} resources
+ * @property {Secrets=} secrets
  */
 export default class App {
   /** @type {Components} */
@@ -41,6 +47,9 @@ export default class App {
 
   /** @type {Resources=} */
   #resources;
+
+  /** @type {Secrets=} */
+  #secrets;
 
   /** @param {AppParams} params */
   constructor({
@@ -81,23 +90,38 @@ export default class App {
   }
 
 
+  #getSecrets() {
+    return this.#secrets;
+  }
+
+  /**
+   * @param {Secrets} secrets
+   */
+  setSecrets(secrets) {
+    this.#secrets = secrets;
+  }
+
+
   async run() {
     const components = this.#components;
     const interfaces = this.#interfaces;
 
     const configs = this.#getConfigs();
     const resources = this.#getResources();
+    const secrets = this.#getSecrets();
 
     await this.#setup({
       components,
       configs: configs?.components,
-      resources: resources?.components
+      resources: resources?.components,
+      secrets: secrets?.components
     });
 
     await this.#runInterfaces({
       interfaces,
       configs: configs?.interfaces,
-      resources: resources?.interfaces
+      resources: resources?.interfaces,
+      secrets: secrets?.interfaces
     });
   }
 
@@ -106,17 +130,20 @@ export default class App {
    * @param {Components} params.components
    * @param {ComponentConfigs=} params.configs
    * @param {ComponentResources=} params.resources
+   * @param {ComponentSecrets=} params.secrets
    */
-  async #setup({ components, configs, resources }) {
+  async #setup({ components, configs, resources, secrets }) {
     await Promise.allSettled(
       Object.entries(components).map(
         ([componentName, component]) => {
           const componentConfigs = configs?.[componentName];
           const componentResources = resources?.[componentName];
+          const componentSecrets = secrets?.[componentName];
 
           component.setup?.({
             configs: componentConfigs,
-            resources: componentResources
+            resources: componentResources,
+            secrets: componentSecrets
           });
         }
       )
@@ -128,16 +155,19 @@ export default class App {
    * @param {Interfaces} params.interfaces
    * @param {InterfaceConfigs=} params.configs
    * @param {InterfaceResources=} params.resources
+   * @param {InterfaceSecrets=} params.secrets
    */
-  async #runInterfaces({ interfaces, configs, resources }) {
+  async #runInterfaces({ interfaces, configs, resources, secrets }) {
     await Promise.allSettled(
       Object.entries(interfaces).map(([interfaceName, appInterface]) => {
         const interfaceConfigs = configs?.[interfaceName];
         const interfaceResources = resources?.[interfaceName];
+        const interfaceSecrets = secrets?.[interfaceName];
 
         return appInterface.run({
           configs: interfaceConfigs,
-          resources: interfaceResources
+          resources: interfaceResources,
+          secrets: interfaceSecrets
         });
       })
     );
